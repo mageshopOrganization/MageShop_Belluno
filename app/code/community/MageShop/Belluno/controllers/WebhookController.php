ce2ef97cc62301585f45bf63d3618b85a920aef6
csvitor <developer.vitorcosta@gmail.com>
2023-03-09 08:57:45 -0300

    ajustando postback

--- app/code/community/MageShop/Belluno/controllers/WebhookController.php
+++ app/code/community/MageShop/Belluno/controllers/WebhookController.php
@@ -16,52 +16,111 @@ class MageShop_Belluno_WebhookController extends MageShop_Belluno_Controller_Abs
 
     public function postbackAction()
     {
-        $post = new Zend_Controller_Request_Http();
-        $data = $post->getRawBody();
-        $data = json_decode($data, true);
+        try {
+            /**
+             * Recebe uma atualização em json
+             */
+            $post = new Zend_Controller_Request_Http();
+            $data = $post->getRawBody();
+            $data = json_decode($data, true);
+            Mage::log( var_export( $data ,true) , Zend_Log::DEBUG , 'mageshop_bulluno_postback.log', true);
+            
+            $orderId = null;
+            $status = null;
+            
+            if(isset($data['transaction']) && count($data['transaction']) > 0){
+                $transactionId = $data['transaction']['transaction_id'];
+                $orderId = $data['transaction']['details'];
+            }else if(isset($data['bankslip']) && count($data['bankslip']) > 0){
+                $transactionId = $data['bankslip']['id'];
+                $orderId = $data['bankslip']['document_code'];
+            }
+    
+            if (empty($orderId) || empty($status)) {
+                return false;
+            }
+            $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
 
-        Mage::log( var_export( $data ,true) , Zend_Log::DEBUG , 'mageshop_bulluno_postback.log', true);
+            if(!$order){
+                return false;
+            }
 
-        $orderId = null;
-        $status = null;
+            /**
+             * Pega o code_payment_method
+             */
+            $payment = $order->getPayment();
+            $method = $payment->getMethod();
+            $uri = $this->_methodPayment($method, $transactionId);
+            $api = $this->getConnector();
 
-        if(isset($data['transaction']) && count($data['transaction']) > 0){
-            $orderId = $data['transaction']['details'];
-            $status = $data['transaction']['status'];
-            $this->comment = $data['transaction']['reason'];
-        }else if(isset($data['bankslip']) && count($data['bankslip']) > 0){
-            $orderId = $data['bankslip']['document_code'];
-            $status = $data['bankslip']['status'];
+            /**
+             * Volta na belluno e confere esse pedido
+             */
+            $resBelluno = json_decode($api->doRequest('', "GET", $uri), true);
+            /**
+             * Gera um log do resultado
+             */
+            Mage::log( var_export( $resBelluno ,true) , Zend_Log::DEBUG , 'mageshop_bulluno_postback_callback.log', true);
+
+            /**
+             * pega o status de retorno
+             */
+            if(isset($resBelluno['transaction']) && count($resBelluno['transaction']) > 0){
+                $status = $resBelluno['transaction']['status'];
+                $this->comment = $resBelluno['transaction']['reason'];
+            }else if(isset($resBelluno['bankslip']) && count($resBelluno['bankslip']) > 0){
+                $status = $resBelluno['bankslip']['status'];
+            }
+
+            /**
+             * Atualiza o pedido no magento
+             */
+            switch ($status) {
+                case self::BL_STATUS_PAID:
+                    parent::payments($resBelluno);
+                    $this->_paid($order, $status);
+                break;
+                case self::BL_STATUS_CC_ANALYSIS:
+                    $this->_review($order, $status);
+                break;
+                case self::BL_STATUS_CC_CLIENT_ANALYSIS:
+                    $this->_holded($order, $status);
+                break;
+                case self::BL_STATUS_REFUSED:
+                case self::BL_STATUS_EXPIRED:
+                case self::BL_STATUS_INACTIVATED:
+                case self::BL_STATUS_CANCELLED:
+                case self::BL_STATUS_BL_CL_BY_DEADLINE:
+                case self::BL_STATUS_BL_CL_BY_REQUEST:
+                case self::BL_STATUS_BL_CL_REQUEST:
+                case self::BL_STATUS_CC_EXPIRED_USER_ANALYSIS:
+                    $this->_cancelled($order, $status);
+                break;
+            }
+        } catch (\Exception $e) {
+           Mage::log( var_export( $e ,true) , Zend_Log::DEBUG , 'mageshop_bulluno_error_postback.log', true);
+           return false;
         }
-        
-        if (empty($orderId) || empty($status)) {
-            return false;
-        }
+    }
 
-        $order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
-        switch ($status) {
-            case self::BL_STATUS_PAID:
-                parent::payments($data);
-                $this->_paid($order, $status);
-            break;
-            case self::BL_STATUS_CC_ANALYSIS:
-                $this->_review($order, $status);
-            break;
-            case self::BL_STATUS_CC_CLIENT_ANALYSIS:
-                $this->_holded($order, $status);
-            break;
-            case self::BL_STATUS_REFUSED:
-            case self::BL_STATUS_EXPIRED:
-            case self::BL_STATUS_INACTIVATED:
-            case self::BL_STATUS_CANCELLED:
-            case self::BL_STATUS_BL_CL_BY_DEADLINE:
-            case self::BL_STATUS_BL_CL_BY_REQUEST:
-            case self::BL_STATUS_BL_CL_REQUEST:
-            case self::BL_STATUS_CC_EXPIRED_USER_ANALYSIS:
-                $this->_cancelled($order, $status);
-            break;
-        }
-
+    private function _methodPayment($key, $transaction_id)
+    {
+      switch ($key) {
+        case 'belluno_creditcard':
+          return "/v2/transaction/{$transaction_id}";
+        break;
+        case 'belluno_bankslip':
+          return "/v2/bankslip/{$transaction_id}";
+        break;
+        case 'belluno_pix':
+          return "/v2/transaction/{$transaction_id}/pix";
+        break;
+      }
     }
     
+    /**Function to return class connector for requests */
+    private function getConnector()
+    {
+        return new MageShop_Belluno_Service_ApiBelluno();
+    }
 }
